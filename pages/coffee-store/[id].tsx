64df809383b88c41a2c3ad30d7fbc47c6eaa587a
at567 <dasarijit727@gmail.com>
2022-11-15 08:51:59 +0530

    coffie store page styleing

--- pages/coffee-store/[id].tsx
+++ pages/coffee-store/[id].tsx
@@ -3,9 +3,16 @@ import Link from 'next/link'
 import  CoffieConisorData  from '../../data/coffee-stores.json';
 import { CardCoffieShopData } from "../../dataModel/cardCoffieShopData";
 import { ParsedUrlQuery } from 'querystring';
-
 import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next';
-  export const getStaticProps: GetStaticProps = async (context:any) => {
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/coffie-store.module.css";
+import cls from "classnames";
+
+interface CoffeStoreProps {
+  CardCoffieShopData:CardCoffieShopData
+  } 
+export const getStaticProps: GetStaticProps = async (context:any) => {
    return {
       props: {CardCoffieShopData:CoffieConisorData.find( coffieStore   => {
      return  coffieStore.id === context.params.id;
@@ -13,26 +20,50 @@ import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next';
     }
   };
   export const getStaticPaths: GetStaticPaths = async () => {
+   const paths = CoffieConisorData.map(CoffeeStore => {
     return {
-        paths: [{ params: { id: '0' } }, { params: { id: '1' } }],
-        fallback: true, // can also be true or 'blocking'
-      }
+    params: {
+      id:CoffeeStore.id,
+    } 
+    }  
+   });
+ 
+   return {
+   paths,
+   fallback: true, // can also be true or 'blocking'
+   }
     
   };
-interface CoffeStoreProps {
-CardCoffieShopData:CardCoffieShopData
-} 
+
 const CoffeeStore:React.FC<CoffeStoreProps> = (({CardCoffieShopData}) => {
 const router = useRouter();
+if(router.isFallback){
+return <div>Loading....</div>;
+}
+
+
 
 console.log("router",CardCoffieShopData);
 return (
-<div>
-coffee store page {router.query.id}
-<Link href="/">Back To Home</Link>ssssss
+<div className={styles.layout} >
+<Head>
+<title>{CardCoffieShopData.name}</title>  
+</Head>
+<div className={styles.cointainer} >
+<div className={styles.col1} >
+<div className={styles.backToHome} >  
+<Link href="/">Back To Home</Link>
+</div>
 <p>{CardCoffieShopData.address}</p>
-<p>{CardCoffieShopData.name}</p>
-<p>{"aaaaaa"}</p>
+</div>
+<div className={styles.col2} >
+<div className={styles.nameWrapper} >  
+<h1 className={styles.name}  >{CardCoffieShopData.name}</h1>
+</div>
+<Image  src={CardCoffieShopData.imgUrl}  width={600} height={300} className={styles.storeImg} alt={CardCoffieShopData.name} />
+<p>{CardCoffieShopData.neighbourhood}</p>
+</div>
+</div>
 </div>
 );
 
